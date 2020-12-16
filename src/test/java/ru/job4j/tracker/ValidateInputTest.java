a8a4d862d965f18fb8f1456e7f17113721390030
julja83 <59063644+julja83@users.noreply.github.com>
2020-12-16 17:19:45 +0300

    Правки 2. Рефакторинг - Шаблон Декоратор для валидатора.

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest extends TestCase {
+ /* @Test
+  public void whenInvalidInput() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+        new String[] {"one", "1"}
+    );
+    ValidateInput input = new ValidateInput(out, in);
+    int selected = input.askInt("Enter menu:");
+    assertThat(selected, is(1));
+  }*/
+}
