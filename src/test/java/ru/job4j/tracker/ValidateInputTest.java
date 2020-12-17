bb1f7e52ce04d519ff0dd4305f10184a439fa0a6
julja83 <59063644+julja83@users.noreply.github.com>
2020-12-17 06:56:22 +0300

    2.1. Тест на ValidateInput

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,20 +1,63 @@
 package ru.job4j.tracker;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-
-import junit.framework.TestCase;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 
-public class ValidateInputTest extends TestCase {
- /* @Test
+public class ValidateInputTest {
+
+  @Test
   public void whenInvalidInput() {
     Output out = new StubOutput();
     Input in = new StubInput(
-        new String[] {"one", "1"}
+        new String[] {"one", "2"}
+    );
+    ValidateInput input = new ValidateInput(out, in);
+    int selected = input.askInt("Enter menu:");
+    assertThat(selected, is(2));
+  }
+
+  @Test
+  public void whenValidInput() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+        new String[] {"1"}
     );
     ValidateInput input = new ValidateInput(out, in);
     int selected = input.askInt("Enter menu:");
     assertThat(selected, is(1));
-  }*/
+  }
+
+  @Test
+  public void whenDifferentInput() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+        new String[] {"ш","к","2","3","3"}
+    );
+    ValidateInput input = new ValidateInput(out, in);
+    int selected = input.askInt("Enter menu:");
+    assertThat(selected, is(2));
+  }
+
+  @Test
+  public void when3ValidInput() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+        new String[] {"2","3","3"}
+    );
+    ValidateInput input = new ValidateInput(out, in);
+    int selected = input.askInt("Enter menu:");
+    assertThat(selected, is(2));
+  }
+
+  @Test
+  public void whenValidNegative1Input() {
+    Output out = new StubOutput();
+    Input in = new StubInput(
+        new String[] {"-1"}
+    );
+    ValidateInput input = new ValidateInput(out, in);
+    int selected = input.askInt("Enter menu:");
+    assertThat(selected, is(-1));//считает валидным,т.к. в этом классе не проверяется это исключение
+  }
 }
