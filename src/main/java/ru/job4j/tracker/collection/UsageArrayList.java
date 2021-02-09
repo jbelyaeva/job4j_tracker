be51223a1015f8008d2d9bba5c841f56e9edf243
julja83 <59063644+julja83@users.noreply.github.com>
2021-02-09 06:35:50 +0300

    0. Коллекции, ArrayList, List, Обобщения

--- src/main/java/ru/job4j/tracker/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/tracker/collection/UsageArrayList.java
@@ -4,4 +4,13 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
 
+  public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<String>();
+    names.add("Petr");
+    names.add("Ivan");
+    names.add("Stepan");
+    for (String name : names) {
+      System.out.println(name);
+    }
+  }
 }
