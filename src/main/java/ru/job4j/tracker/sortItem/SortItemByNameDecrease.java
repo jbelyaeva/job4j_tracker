9b875d44dd243c185e5e4f0d66f5613fd06698c8
julja83 <59063644+julja83@users.noreply.github.com>
2021-03-15 11:12:18 +0300

    Правки 0. Сортировка

--- src/main/java/ru/job4j/tracker/sortItem/SortItemByNameDecrease.java
+++ src/main/java/ru/job4j/tracker/sortItem/SortItemByNameDecrease.java
@@ -6,44 +6,8 @@ import ru.job4j.tracker.Item;
 
 public class SortItemByNameDecrease implements Comparator<Item> {
 
-  private int id;
-  private String name;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public String toString() {
-    return "Item{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        '}';
-  }
-
   @Override
   public int compare(Item first, Item second) {
       return second.getName().compareTo(first.getName());
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    SortItemByNameDecrease that = (SortItemByNameDecrease) o;
-    return id == that.id && Objects.equals(name, that.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
   }
 }
--- src/main/java/ru/job4j/tracker/sortItem/SortItemByNameIncrease.java
+++ src/main/java/ru/job4j/tracker/sortItem/SortItemByNameIncrease.java
@@ -1,49 +1,12 @@
 package ru.job4j.tracker.sortItem;
 
 import java.util.Comparator;
-import java.util.Objects;
 import ru.job4j.tracker.Item;
 
 public class SortItemByNameIncrease implements Comparator<Item> {
 
-  private int id;
-  private String name;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  @Override
-  public String toString() {
-    return "Item{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        '}';
-  }
-
   @Override
   public int compare(Item first, Item second) {
-      return first.getName().compareTo(second.getName());
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    SortItemByNameIncrease that = (SortItemByNameIncrease) o;
-    return id == that.id && Objects.equals(name, that.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
+    return first.getName().compareTo(second.getName());
   }
 }
